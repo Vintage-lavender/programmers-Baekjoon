e7163f218719c73ed6dcb6d1d539699d2776d998
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-05 02:39:17 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -0,0 +1,15 @@
+def solution(N, stages):
+    answer = [] #단계별로 (실패율, 단계)로 저장
+    slist = [0] * 503
+    for s in stages:
+        slist[s] += 1
+    people = len(stages) #남은 사람 수
+    for i in range(1,N+1):
+        p = 0
+        if people != 0:
+            p = slist[i]/people
+        answer.append((i,p))
+        people -= slist[i] #남은 사람 수 갱신
+    #단계순으로 리스트에 삽입했으므로 단계별로 오름차순 정렬이 이미 되어 있음.
+    #실패율이 큰 것 부터 내림차순 정렬 후 단계만 출력
+    return list(map(lambda x: x[0] ,sorted(answer, key=lambda x: x[1], reverse=True)))
