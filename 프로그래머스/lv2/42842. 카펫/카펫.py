ce4d7419b1720b6cb512e4b803590f395e6c2794
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-07 18:14:17 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,9 @@
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow
+    for h in range(3,int(total**0.5)+1): #세로 길이만 탐색
+        if total%h == 0:
+            w = total//h
+            if (w-2)*(h-2) == yellow:
+                answer += [w,h]
+    return answer
