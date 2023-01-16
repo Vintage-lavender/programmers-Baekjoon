f6eacb45b2b9d42ad4111692516c6ffc73074b8f
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-17 01:03:02 +0900

    [level 1] Title: 모의고사, Time: 1.86 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -2,25 +2,22 @@
 #1. 1,2,3,4,5
 #2. 2,1,2,3,2,4,2,5
 #3. 3,3,1,1,2,2,4,4,5,5
+from itertools import cycle
 def solution(answers):
-    qnum = len(answers)
-    fstud = [1,2,3,4,5] * (qnum//5+1)
-    sstud = [2,1,2,3,2,4,2,5] * (qnum//8+1)
-    tstud = [3,3,1,1,2,2,4,4,5,5] * (qnum//10+1)
-    cnt = {1:0,2:0,3:0}
-    for i in range(qnum):
-        if answers[i] == fstud[i]:
+    fstud = cycle([1,2,3,4,5])
+    sstud = cycle([2,1,2,3,2,4,2,5])
+    tstud = cycle([3,3,1,1,2,2,4,4,5,5])
+    cnt = [0] * 3
+    for a in answers:
+        if a == next(fstud):
+            cnt[0] += 1
+        if a == next(sstud):
             cnt[1] += 1
-        if answers[i] == sstud[i]:
+        if a == next(tstud):
             cnt[2] += 1
-        if answers[i] == tstud[i]:
-            cnt[3] += 1
-    cnt = sorted(cnt.items(),key=lambda x: x[1],reverse=True)
-    minvalue = cnt[0][1]
+    maxvalue = max(cnt)
     final = []
-    for i,c in cnt:
-        if minvalue == c:
-            final.append(i)
-        else:
-            return final
+    for i,c in enumerate(cnt):
+        if maxvalue == c:
+            final.append(i+1)
     return final
