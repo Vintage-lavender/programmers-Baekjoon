1c0b83e1b7b382597dc2ed3dd810df2c72bc4e40
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-17 01:07:49 +0900

    [level 1] Title: 모의고사, Time: 4.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -4,17 +4,14 @@
 #3. 3,3,1,1,2,2,4,4,5,5
 from itertools import cycle
 def solution(answers):
-    fstud = cycle([1,2,3,4,5])
-    sstud = cycle([2,1,2,3,2,4,2,5])
-    tstud = cycle([3,3,1,1,2,2,4,4,5,5])
+    stud = [cycle([1,2,3,4,5]),
+            cycle([2,1,2,3,2,4,2,5]),
+            cycle([3,3,1,1,2,2,4,4,5,5])]
     cnt = [0] * 3
     for a in answers:
-        if a == next(fstud):
-            cnt[0] += 1
-        if a == next(sstud):
-            cnt[1] += 1
-        if a == next(tstud):
-            cnt[2] += 1
+        for i in range(3):
+            if a == next(stud[i]):
+                cnt[i] += 1
     maxvalue = max(cnt)
     final = []
     for i,c in enumerate(cnt):
