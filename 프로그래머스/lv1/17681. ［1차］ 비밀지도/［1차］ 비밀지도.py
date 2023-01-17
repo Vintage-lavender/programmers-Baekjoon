6898cbfc8329fd1a0c7f085fba5f91a904654b8b
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-18 03:10:52 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,20 +2,8 @@
 def solution(n, arr1, arr2):
     answer = []
     for a1,a2 in zip(arr1,arr2):
-        str1 = ''
-        str2 = ''
-        i = 2**n
-        while i%2==0:
-            i //= 2
-            str1 += str(a1//i)
-            str2 += str(a2//i)
-            a1 -= i * (a1//i)
-            a2 -= i * (a2//i)
-        a = ''
-        for s1,s2 in zip(str1,str2):
-            if int(s1) or int(s2):
-                a += '#'
-            else:
-                a += ' '
-        answer.append(a)
+        row = bin(a1|a2)[2:].rjust(n,'0')
+        row = row.replace('1','#')
+        row = row.replace('0',' ')
+        answer.append(row)
     return answer
