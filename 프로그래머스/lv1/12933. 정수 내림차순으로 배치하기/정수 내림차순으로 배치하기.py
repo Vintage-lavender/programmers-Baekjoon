5100996ccc5ce2ef66eb317a00158ea20f4d1308
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-18 12:23:54 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,4 +1,4 @@
 def solution(n):
-    #print(list(str(n)))
-    #리스트에 스트링을 그대로 넣으면 문자한개씩 분리됨
+    #print(sorted(str(n))) #sorted에 스트링을 그대로 넣으면 모든 문자가 정렬됨
+    #리스트에 스트링을 그대로 넣으면 문자한개씩 분리됨 list(str(n))
     return int(''.join(sorted(map(str,str(n)),reverse=True)))
