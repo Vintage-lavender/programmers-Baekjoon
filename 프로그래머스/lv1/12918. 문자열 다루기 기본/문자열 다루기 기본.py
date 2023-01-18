1a9f8b3ed6cbf87cfbba3f0eca37c667e925ef79
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-19 00:28:49 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,3 +1,4 @@
 import re
 def solution(s):
-    return len(re.sub('\D',"",s)) == len(s) and (len(s) in [4,6]) #문자(\D)빼고 정수만 남기기
+    #return len(re.sub('\D',"",s)) == len(s) and (len(s) in [4,6]) #문자(\D)빼고 정수만 남기기
+    return s.isdigit() and (len(s) in [4,6])
