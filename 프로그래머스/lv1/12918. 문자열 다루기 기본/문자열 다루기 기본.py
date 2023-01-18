a486935a05d294ba2dabdf5326dca0928daad553
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-19 00:32:34 +0900

    Update 문자열 다루기 기본.py

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,4 +1,17 @@
-import re
 def solution(s):
-    #return len(re.sub('\D',"",s)) == len(s) and (len(s) in [4,6]) #문자(\D)빼고 정수만 남기기
+    
+    '''
+    #첫번째 방법
+    try:
+        int(s)
+        return len(s) in [4,6]
+    except:
+        return False
+    '''
+    '''
+    #두번째 방법
+    import re
+    return len(re.sub('\D',"",s)) == len(s) and (len(s) in [4,6]) #문자(\D)빼고 정수만 남기기
+    '''
+    #가장 시간이 적게 걸리는 세번째 방법
     return s.isdigit() and (len(s) in [4,6])
