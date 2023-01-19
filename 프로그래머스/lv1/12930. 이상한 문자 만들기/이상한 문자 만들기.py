2dce36db7021f98f395ec46d13c90ec9f0c8be40
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-20 01:39:41 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    slist = s.split(' ')
+    for word in slist:
+        word = list(word.lower())
+        for i,w in enumerate(word):
+            if i%2==0:
+                word[i] = chr(ord(w)-32)
+        answer+=''.join(word)+ ' '
+    return answer[:-1]
