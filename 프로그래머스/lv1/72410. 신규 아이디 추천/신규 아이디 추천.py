b0e359404127283992b5864e653ddd220f742162
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-20 01:20:47 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.37 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,24 @@
+import re
+def solution(new_id):
+    new_id = new_id.lower()
+    sym = re.compile('[a-z0-9-_.]') #보관할 것
+
+    new_id = ''.join(sym.findall(new_id)).split('.')
+    answer = ''
+    for i in new_id:
+        if i:
+            answer += i+'.'
+    new_id = answer.rstrip('.')
+    
+    print(new_id)
+    
+    if not new_id:
+        new_id += 'aaa'
+    elif len(new_id) >= 16:
+        new_id = new_id[:15].rstrip('.')
+    elif len(new_id) <= 2:
+        while len(new_id)<3:
+            new_id += new_id[-1]
+    
+
+    return new_id
