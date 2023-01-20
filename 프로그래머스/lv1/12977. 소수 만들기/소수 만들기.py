103071b35fbf5629b27d97aab0d08b91ce71d623
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-20 18:03:45 +0900

    [level 1] Title: 소수 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -3,18 +3,10 @@ def solution(nums):
     for i in range(len(nums)-2):
         for j in range((i+1),(len(nums)-1)):
             for k in range((j+1),len(nums)):
-                cand.append(nums[i]+nums[j]+nums[k])
-    #print(cand)
-    index = 0
-    while index<len(cand):
-        c = cand[index]
-        i = 2
-        while c%i:
-            i+=1
-        #print(c, i)
-        if i!=c:
-            cand.remove(c)
-            continue
-        index += 1
-    #print(cand)
+                comb = nums[i]+nums[j]+nums[k]
+                n = 2
+                while comb%n:
+                    n+=1
+                if comb==n:
+                    cand.append(comb)
     return len(cand)
