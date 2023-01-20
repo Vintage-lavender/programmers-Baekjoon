ae50e6ac03f9ef290f8b7f8eaad4fbd8e2e108f9
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-20 18:21:20 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,22 @@
+#작은 것의 약수 구하고, 큰 것의 배수구하기(n*m에서 n의 크기 줄여가면서 배수 확인)
+def solution(n, m):
+    answer = []
+    if n>m:
+        n,m = m,n
+    #n의 약수 구하기
+    largest = 1
+    for i in range(1,int(n**0.5)+1):
+        if n%i==0:
+            if m%(n//i)==0:
+                largest = n//i
+                break
+            elif m%i==0:
+                largest = i
+    smallest = m
+    while smallest <= n*m:
+        if (smallest%n)==0:
+            break
+        else:
+            smallest += m
+                
+    return [largest,smallest]
