3d9a157fc2a6d0f1db967f1890f6fd3ab205f5b1
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-20 22:42:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 141.25 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,6 @@
-from collections import deque
 def solution(arr):
-    answer = deque()
-    answer.append(arr[0])
-    for i in arr[1:]:
-        if answer[-1]!=i:
-            answer.append(i)
-    return list(answer)
+    a = []
+    for i in arr:
+        if a[-1:] == [i]: continue
+        a.append(i)
+    return a
