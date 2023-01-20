691a3d0db13307d062286ccd54d7e97a44226d11
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-20 22:25:56 +0900

    [level 1] Title: 소수 찾기, Time: 332.27 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,4 +1,10 @@
 def solution(n):
+    num=set(range(2,n+1))
+    for i in range(2,n+1):
+        if i in num: #이전 차집합 과정을 거친 후에도 남아있는 수에 대하여만 수행
+            num-=set(range(2*i,n+1,i)) #i의 배수를 모두 빼줌
+    return len(num)
+'''
     answer = 1
     for i in range(3,n+1):
         cnt = 0
@@ -9,3 +15,4 @@ def solution(n):
         if cnt==0:
             answer += 1
     return answer
+'''
