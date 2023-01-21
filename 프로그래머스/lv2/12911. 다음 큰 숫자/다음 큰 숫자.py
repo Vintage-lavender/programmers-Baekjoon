90ec277384387b320a447370a814a3cab72e524d
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-21 20:09:33 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,5 +1,7 @@
-#뒤에서부터 0의 인덱스 찾기. 0의 인덱스 다음 인덱스의 값이 1일때 자리 교체
+
 def solution(n):
+    '''
+    #뒤에서부터 0의 인덱스 찾기. 0의 인덱스 다음 인덱스의 값이 1일때 자리 교체
     nlist = list(map(str,'0'+bin(n)[2:]))
     #print(nlist)
     for i in range(len(nlist)-1,0,-1):
@@ -7,5 +9,12 @@ def solution(n):
             nlist[i], nlist[i-1] = nlist[i-1], nlist[i]
             nlist[i:] = sorted(nlist[i:])
             return int(''.join(nlist),2)
+    '''
+    num1 = bin(n).count('1') #이진수의 1개수 세기
+    while True:
+        n +=1 #1씩 늘려가며 n보다 큰 수 중에서 이진수의 1개수가 n과 같은 것 찾기
+        if num1 == bin(n).count('1'): #갱신된 n의 1개수
+            break
+    return n
     
         
