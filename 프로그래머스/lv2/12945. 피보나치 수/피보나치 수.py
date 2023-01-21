379735c07ae401f5b918f6691010a5f3c00bc146
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-21 17:26:55 +0900

    [level 2] Title: 피보나치 수, Time: 81.69 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,4 +1,7 @@
+    
 def solution(n):
+    '''
+    #dp
     fibo = [0] * (n+1)
     fibo[1] = 1
     i = 2
@@ -6,3 +9,8 @@ def solution(n):
         fibo[i] = fibo[i-1]+fibo[i-2]
         i+=1
     return fibo[n]%1234567
+    '''
+    a,b = 0,1
+    for _ in range(n-1):
+        a,b = b,a+b
+    return b%1234567
