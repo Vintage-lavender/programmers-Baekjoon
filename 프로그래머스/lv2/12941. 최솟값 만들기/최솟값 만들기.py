fe058f8be31dceaf61fbbeb60064ec4672e43422
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-21 16:02:49 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.39 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,3 +1,4 @@
 def solution(A,B):
     #return sum(map(lambda x: x[0]*x[1],zip(sorted(A),sorted(B,reverse=True))))
-    return sum(map(lambda a,b : a*b, sorted(A), sorted(B, reverse=True)))
+    #return sum(map(lambda a,b : a*b, sorted(A), sorted(B, reverse=True)))
+    return sum(a*b for a,b in zip(sorted(A), sorted(B, reverse=True)))
