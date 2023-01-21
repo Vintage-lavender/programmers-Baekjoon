269e620da9be47c0ed60d35531aef4973e845b06
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-22 03:46:50 +0900

    [level 2] Title: 구명보트, Time: 7.47 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,6 +1,7 @@
 #최대한 보트에 두명을 모두 태울 수 있는 방향으로.
-#스택
 def solution(people, limit):
+    '''
+    #우선순위 큐
     from collections import deque
     answer = 0
     #people.sort(reverse=True)
@@ -14,3 +15,15 @@ def solution(people, limit):
             queue.popleft()
         answer += 1
     return answer
+    '''
+    #투 포인터
+    answer = 0
+    people.sort()
+    i = 0
+    j = len(people)-1
+    while i<j:
+        if people[i]+people[j] <= limit:
+            answer += 1 #둘이 같이 탈 수 있는 경우
+            i += 1
+        j -= 1
+    return len(people)-answer #전체 사람 수 중에서 둘이 같이타는 경우 빼기
