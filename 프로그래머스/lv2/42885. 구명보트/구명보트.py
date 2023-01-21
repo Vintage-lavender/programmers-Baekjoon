72e164d72defcbc43492c86bf66c282bffcb5896
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-22 01:47:17 +0900

    [level 2] Title: 구명보트, Time: 12.95 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+#최대한 보트에 두명을 모두 태울 수 있는 방향으로.
+#스택
+def solution(people, limit):
+    from collections import deque
+    answer = 0
+    people.sort(reverse=True)
+    queue = deque(map(int,people))
+    while queue:
+        if len(queue)>1 and queue[0]+queue[-1]<=limit:
+            queue.pop()
+            queue.popleft()
+        else:
+            queue.popleft()
+        answer += 1
+    return answer
