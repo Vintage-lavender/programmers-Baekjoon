acd149d364fbe424db34d6ab0b69fb5907373ba2
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-21 16:36:13 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.14 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    cnt = 0
+    zlen = 0
+    while s!='1':
+        new = len(s.replace('0',''))
+        zlen += len(s) - new
+        s = bin(new)[2:]
+        cnt += 1
+    return [cnt,zlen] #변환횟수, 제거된 0개수
