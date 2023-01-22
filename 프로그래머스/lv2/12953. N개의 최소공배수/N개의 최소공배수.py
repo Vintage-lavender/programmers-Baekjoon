a91bec6a16bc865560b69ddb963571f5ea5fd833
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-23 02:27:21 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,5 +1,10 @@
 #앞의 두 수를 비교하면서 최소공배수를 갱신
-def lcm(a,b):
+def gcd(a,b): #a>=b
+    print(a,b)
+    if b%a==0:
+        return a
+    return gcd(b%a,a)
+    '''
     a,b = min(a,b), max(a,b)
     maximum = 1
     for i in range(1,int(a**0.5)+1):
@@ -9,10 +14,12 @@ def lcm(a,b):
             if b%(a//i)==0:
                 return (a*b)//(a//i)
     return (a*b)//maximum
+    '''
 
 def solution(arr):
     arr.sort()
-    l = arr[0]
+    x = arr[0]
     for i in range(1,len(arr)):
-        l = lcm(l,arr[i])
-    return l
+        x,y = min(x,arr[i]), max(x,arr[i])
+        x = (x*y)//gcd(x,y)
+    return x
