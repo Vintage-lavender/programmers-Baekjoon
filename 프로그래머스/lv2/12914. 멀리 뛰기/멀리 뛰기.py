c8478dddd07375fd874f441e8fc52a383406f7b7
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-23 16:46:36 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,13 +1,16 @@
 def solution(n):
-    
+    '''
     dp = [1]*(n+1)
     for i in range(2,n+1):
         dp[i] = dp[i-2]+dp[i-1]
     return dp[n]%1234567
     '''
+    #print(40)
     a,b = 1,1
-    while b<n:
-        temp = a+b
-        a,b = b, temp
-    return b
-    '''
+    #print(a,b)
+    #while b<40: #n이 커지면 경우의 수가 n보다 커지는 것은 매우 당연하므로 이 코드는 틀렸음
+    for i in range(n-1):
+        a,b = b,(a+b)
+        #print(a,b)
+    return b%1234567
+    
