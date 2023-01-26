6de8d8e2eccba891b63597a7c188bcb89e36273f
Vintage-lavender <96819499+Vintage-lavender@users.noreply.github.com>
2023-01-26 17:50:08 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -2,6 +2,15 @@
 #h: 인용횟수
 #h이상 인용된 논문은 h≤개, h이하 인용된 논문은 h≥개
 def solution(citations):
+    citations.sort()
+    papers = len(citations)
+    for i,c in enumerate(citations): #최댓값, h값의 경계는 인용횟수 중에서 결정됨.
+        if c >= papers-i: #인덱스를 그대로 빼주면 c이하로 인용된 논문을 빼고 셀 수 있음.
+            return papers-i
+    return 0 #h값을 찾지 못한 경우 h=0
+
+    
+    '''
     answer = 0
     #인용횟수 리스트
     cnt = [0] * 10001
@@ -17,5 +26,5 @@ def solution(citations):
             answer = mid
             start = mid+1
         else:
-            end = mid-1
+            end = mid-1'''
     return answer
